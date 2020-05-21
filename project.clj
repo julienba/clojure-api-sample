816afb2f6781dd6895e1fa323dcc30aeb91815ae
Julien Bille <julien.bille@elmar.nl>
2020-05-21 11:39:49 +0200

    Add comments

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject app "0.1.0-SNAPSHOT"
-  :description "Sample API"
+  :description "API Sample using reitit and mount"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
--- src/app/config.clj
+++ src/app/config.clj
@@ -3,9 +3,12 @@
             [clojure.java.io :as io]
             [mount.core :refer [defstate]]))
 
-(defn read-config []
+(defn read-config
+  "Read the configuration resources/config.edn"
+  []
   (-> "config.edn"
       io/resource
       (aero/read-config)))
 
+; Store the configuration in a mount state to be available from other part of your application
 (defstate config :start (read-config))
